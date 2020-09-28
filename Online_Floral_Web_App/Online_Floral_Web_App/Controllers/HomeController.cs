94b00064486cb965810b64cc32a76901bac2bf0e
MehwishAshraf <68462009+MehwishAshraf@users.noreply.github.com>
2020-09-28 09:11:27 +0500

    final

--- Online_Floral_Web_App/Online_Floral_Web_App/Controllers/HomeController.cs
+++ Online_Floral_Web_App/Online_Floral_Web_App/Controllers/HomeController.cs
@@ -14,8 +14,19 @@ namespace Online_Floral_Web_App.Controllers
             return View();
         }
 
-        public ActionResult Category()
+        public ActionResult Category(int id)
         {
+            ViewBag.cate = "";
+            switch (id)
+            {
+                case 1: ViewBag.cate = "Birthday"; break;
+                case 2: ViewBag.cate = "Father's Day"; break;
+                case 3: ViewBag.cate = "Mother's Day"; break;
+                case 4: ViewBag.cate = "Baby New Arrival"; break;
+                case 5: ViewBag.cate = "Congratulation"; break;
+                case 6: ViewBag.cate = "Anniversary"; break;
+
+            }
             return View();
         }
 
